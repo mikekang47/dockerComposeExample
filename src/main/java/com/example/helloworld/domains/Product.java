faca985bbe9685cccd4af51333f8a387e03d260a
mikekang47 <gyeong0308@gmail.com>
2022-12-08 22:13:29 +0900

    Feat: update deployment.yml

--- src/main/java/com/example/helloworld/domains/Product.java
+++ src/main/java/com/example/helloworld/domains/Product.java
@@ -1,24 +1,23 @@
 package com.example.helloworld.domains;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Table(name = "product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
     public Product(String name) {
